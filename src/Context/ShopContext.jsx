9855faddcc37945f1e42581fbfa96be748ea1e1b
Willy Gafabusa <willygafabusa@example.com>
2024-04-02 15:38:00 +0300

    first commit

---
+++ src/Context/ShopContext.jsx
@@ -0,0 +1,55 @@
+import React, { createContext, useState } from "react";
+import all_product from "../Components/Assets/all_product";
+import CartItems from "../Components/CartItems/CartItems";
+
+export const ShopContext = createContext(null);
+
+const getDefaultCart=()=>{
+  let cart={};
+  for (let index=0; index <all_product.length+1; index++){
+    cart[index]=0;
+  }
+  return cart;
+} 
+
+const ShopContextProvider=(props)=> {
+  
+//variable for adding items to the cart 
+  const [cartItems,setCartItems]=useState(getDefaultCart());
+
+  const addToCart=(itemId)=>{
+    setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}));
+    console.log(cartItems);
+  }
+//Removing items from the cart.
+  const removeFromCart=(itemId)=>{
+    setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
+  }
+  const getTotalCartAmount=()=>{
+    let totalAmount=0;
+    for (const item in cartItems){
+      if (cartItems[item]>0){
+        let itemInfo=all_product.find((product)=>product.id===Number(item))
+        totalAmount +=CartItems[item] * itemInfo.new_price;
+      }
+    }
+    return totalAmount;
+  }
+  const getTotalCartItems=()=>{
+    let totalItem=0;
+    for (const item in cartItems){
+      if (cartItems[item]>0){
+        totalItem+=cartItems[item];
+      }
+    }
+    return totalItem;
+  }
+  const contextValue={getTotalCartItems,getTotalCartAmount,all_product,cartItems,addToCart,removeFromCart};
+  return (
+    <ShopContext.Provider value={contextValue}>
+      {props.children}
+    </ShopContext.Provider>
+  )
+}
+
+export default ShopContextProvider;
